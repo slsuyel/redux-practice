53149d6418fe6d5039b81f135d059c11096cb5bf
Suyel Haque <slsuyel@gmail.com>
2024-02-09 22:46:45 +0600

    Simple todo app done with redux

--- src/pages/Home/TodoPage.tsx
+++ src/pages/Home/TodoPage.tsx
@@ -4,7 +4,7 @@ import Container from "@/components/ui/Container";
 const TodoPage = () => {
   return (
     <Container>
-      <h1 className="text-center font-semibold text-3xl my-10">My Todos</h1>
+      <h1 className="text-center font-semibold text-3xl my-10">My Todo App</h1>
       <TodoContainer />
     </Container>
   );
